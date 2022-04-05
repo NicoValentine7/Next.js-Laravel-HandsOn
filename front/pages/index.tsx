572382494064aa93f204f69c6df485d3c7ee07dd
NicoValentine7 <y.elucidator@gmail.com>
2022-04-05 19:09:58 +0900

    見本コピー

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,22 +1,39 @@
-import { AxiosError, AxiosResponse } from 'axios';
 import type { NextPage } from 'next';
+import { AxiosError, AxiosResponse } from 'axios';
 import { ChangeEvent, useState } from 'react';
 import { RequiredMark } from '../components/RequiredMark';
 import { axiosApi } from '../lib/axios';
+import { useRouter } from 'next/router';
 
+// POSTデータの型
 type LoginForm = {
   email: string;
   password: string;
-}
+};
 
+// バリデーションメッセージの型
 type Validation = LoginForm & { loginFailed: string };
 
 const Home: NextPage = () => {
+  // ルーター定義
+  const router = useRouter();
+  // state定義
   const [loginForm, setLoginForm] = useState<LoginForm>({
     email: '',
-    password: ''
-  })
+    password: '',
+  });
+  const [validation, setValidation] = useState<Validation>({
+    email: '',
+    password: '',
+    loginFailed: '',
+  });
 
+  // POSTデータの更新
+  const updateLoginForm = (e: ChangeEvent<HTMLInputElement>) => {
+    setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
+  };
+
+  // ログイン
   const login = () => {
     axiosApi
       // CSRF保護の初期化
@@ -27,16 +44,13 @@ const Home: NextPage = () => {
           .post('/login', loginForm)
           .then((response: AxiosResponse) => {
             console.log(response.data);
+            router.push('/memos');
           })
           .catch((err: AxiosError) => {
             console.log(err.response);
           });
       });
-    };
-
-const updateLoginForm = (e: ChangeEvent<HTMLInputElement>) => {
-  setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
-};
+  };
 
   return (
     <div className='w-2/3 mx-auto py-24'>
@@ -50,6 +64,8 @@ const updateLoginForm = (e: ChangeEvent<HTMLInputElement>) => {
           <input
             className='p-2 border rounded-md w-full outline-none'
             name='email'
+            value={loginForm.email}
+            onChange={updateLoginForm}
           />
           {/* <p className='py-3 text-red-500'>必須入力です。</p> */}
         </div>
@@ -65,6 +81,8 @@ const updateLoginForm = (e: ChangeEvent<HTMLInputElement>) => {
             className='p-2 border rounded-md w-full outline-none'
             name='password'
             type='password'
+            value={loginForm.password}
+            onChange={updateLoginForm}
           />
           {/* <p className='py-3 text-red-500'>
             8文字以上の半角英数字で入力してください。
@@ -74,7 +92,10 @@ const updateLoginForm = (e: ChangeEvent<HTMLInputElement>) => {
           {/* <p className='py-3 text-red-500'>
             IDまたはパスワードが間違っています。
           </p> */}
-          <button className='bg-gray-700 text-gray-50 py-3 sm:px-20 px-10 rounded-xl cursor-pointer drop-shadow-md hover:bg-gray-600'>
+          <button
+            className='bg-gray-700 text-gray-50 py-3 sm:px-20 px-10 rounded-xl cursor-pointer drop-shadow-md hover:bg-gray-600'
+            onClick={login}
+          >
             ログイン
           </button>
         </div>
