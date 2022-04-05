a082efb00aa404e1dace3825937a5127072a5bf3
NicoValentine7 <y.elucidator@gmail.com>
2022-04-05 21:40:53 +0900

    Sample init

--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -1,9 +1,12 @@
 import type { NextPage } from 'next';
 import { AxiosError, AxiosResponse } from 'axios';
-import { ChangeEvent, useState } from 'react';
+import { useState } from 'react';
 import { RequiredMark } from '../components/RequiredMark';
 import { axiosApi } from '../lib/axios';
 import { useRouter } from 'next/router';
+import { useUserState } from '../atoms/userAtom';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 
 // POSTデータの型
 type LoginForm = {
@@ -12,42 +15,60 @@ type LoginForm = {
 };
 
 // バリデーションメッセージの型
-type Validation = LoginForm & { loginFailed: string };
+type Validation = {
+  email?: string;
+  password?: string;
+  loginFailed?: string;
+};
 
 const Home: NextPage = () => {
   // ルーター定義
   const router = useRouter();
   // state定義
-  const [loginForm, setLoginForm] = useState<LoginForm>({
-    email: '',
-    password: '',
-  });
-  const [validation, setValidation] = useState<Validation>({
-    email: '',
-    password: '',
-    loginFailed: '',
-  });
+  const [validation, setValidation] = useState<Validation>({});
+  // recoil stateの呼び出し
+  const { setUser } = useUserState();
 
-  // POSTデータの更新
-  const updateLoginForm = (e: ChangeEvent<HTMLInputElement>) => {
-    setLoginForm({ ...loginForm, [e.target.name]: e.target.value });
-  };
+  // React-Hook-Form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginForm>();
 
   // ログイン
-  const login = () => {
+  const login = (data: LoginForm) => {
+    // バリデーションメッセージの初期化
+    setValidation({});
+
     axiosApi
       // CSRF保護の初期化
       .get('/sanctum/csrf-cookie')
       .then((res) => {
         // ログイン処理
         axiosApi
-          .post('/login', loginForm)
+          .post('/login', data)
           .then((response: AxiosResponse) => {
-            console.log(response.data);
+            setUser(response.data.data);
             router.push('/memos');
           })
           .catch((err: AxiosError) => {
             console.log(err.response);
+            // バリデーションエラー
+            if (err.response?.status === 422) {
+              const errors = err.response?.data.errors;
+              // state更新用のオブジェクトを別で定義
+              const validationMessages: { [index: string]: string } =
+                {} as Validation;
+              Object.keys(errors).map((key: string) => {
+                validationMessages[key] = errors[key][0];
+              });
+              // state更新用オブジェクトに更新
+              setValidation(validationMessages);
+            }
+            if (err.response?.status === 500) {
+              alert('システムエラーです！！');
+            }
           });
       });
   };
@@ -63,11 +84,24 @@ const Home: NextPage = () => {
           </div>
           <input
             className='p-2 border rounded-md w-full outline-none'
-            name='email'
-            value={loginForm.email}
-            onChange={updateLoginForm}
+            {...register('email', {
+              required: '必須入力です。',
+              pattern: {
+                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                message: '有効なメールアドレスを入力してください。',
+              },
+            })}
           />
-          {/* <p className='py-3 text-red-500'>必須入力です。</p> */}
+          <ErrorMessage
+            errors={errors}
+            name={'email'}
+            render={({ message }) => (
+              <p className='py-3 text-red-500'>{message}</p>
+            )}
+          />
+          {validation.email && (
+            <p className='py-3 text-red-500'>{validation.email}</p>
+          )}
         </div>
         <div className='mb-5'>
           <div className='flex justify-start my-2'>
@@ -79,22 +113,33 @@ const Home: NextPage = () => {
           </small>
           <input
             className='p-2 border rounded-md w-full outline-none'
-            name='password'
             type='password'
-            value={loginForm.password}
-            onChange={updateLoginForm}
+            {...register('password', {
+              required: '必須入力です。',
+              pattern: {
+                value: /^([a-zA-Z0-9]{8,})$/,
+                message: '8文字以上の半角英数字で入力してください',
+              },
+            })}
           />
-          {/* <p className='py-3 text-red-500'>
-            8文字以上の半角英数字で入力してください。
-          </p> */}
+          <ErrorMessage
+            errors={errors}
+            name={'password'}
+            render={({ message }) => (
+              <p className='py-3 text-red-500'>{message}</p>
+            )}
+          />
+          {validation.password && (
+            <p className='py-3 text-red-500'>{validation.password}</p>
+          )}
         </div>
         <div className='text-center mt-12'>
-          {/* <p className='py-3 text-red-500'>
-            IDまたはパスワードが間違っています。
-          </p> */}
+          {validation.loginFailed && (
+            <p className='py-3 text-red-500'>{validation.loginFailed}</p>
+          )}
           <button
             className='bg-gray-700 text-gray-50 py-3 sm:px-20 px-10 rounded-xl cursor-pointer drop-shadow-md hover:bg-gray-600'
-            onClick={login}
+            onClick={handleSubmit(login)}
           >
             ログイン
           </button>
--- front/pages/memos/post.tsx
+++ front/pages/memos/post.tsx
@@ -1,7 +1,86 @@
+import { AxiosError, AxiosResponse } from 'axios';
 import type { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 import { RequiredMark } from '../../components/RequiredMark';
+import { useAuth } from '../../hooks/useAuth';
+import { axiosApi } from '../../lib/axios';
+
+// POSTデータの型
+type MemoForm = {
+  title: string;
+  body: string;
+};
+
+// バリデーションメッセージの型
+type Validation = {
+  title?: string;
+  body?: string;
+};
 
 const Post: NextPage = () => {
+  // ルーター定義
+  const router = useRouter();
+  // state定義
+  const [validation, setValidation] = useState<Validation>({});
+  const { checkLoggedIn } = useAuth();
+
+  // React-Hook-Form
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<MemoForm>();
+
+  useEffect(() => {
+    const init = async () => {
+      // ログイン中か判定
+      const res: boolean = await checkLoggedIn();
+      if (!res) {
+        router.push('/');
+      }
+    };
+    init();
+  }, []);
+
+  // メモの登録
+  const createMemo = (data: MemoForm) => {
+    // バリデーションメッセージの初期化
+    setValidation({});
+
+    axiosApi
+      // CSRF保護の初期化
+      .get('/sanctum/csrf-cookie')
+      .then((res) => {
+        // APIへのリクエスト
+        axiosApi
+          .post('/api/memos', data)
+          .then((response: AxiosResponse) => {
+            console.log(response.data);
+            router.push('/memos');
+          })
+          .catch((err: AxiosError) => {
+            // バリデーションエラー
+            if (err.response?.status === 422) {
+              const errors = err.response?.data.errors;
+              // state更新用のオブジェクトを別で定義
+              const validationMessages: { [index: string]: string } =
+                {} as Validation;
+              Object.keys(errors).map((key: string) => {
+                validationMessages[key] = errors[key][0];
+              });
+              // state更新用オブジェクトに更新
+              setValidation(validationMessages);
+            }
+            if (err.response?.status === 500) {
+              alert('システムエラーです！！');
+            }
+          });
+      });
+  };
+
   return (
     <div className='w-2/3 mx-auto'>
       <div className='w-1/2 mx-auto mt-32 border-2 px-12 py-16 rounded-2xl'>
@@ -13,8 +92,18 @@ const Post: NextPage = () => {
           </div>
           <input
             className='p-2 border rounded-md w-full outline-none'
-            name='title'
+            {...register('title', { required: '必須入力です。' })}
           />
+          <ErrorMessage
+            errors={errors}
+            name={'title'}
+            render={({ message }) => (
+              <p className='py-3 text-red-500'>{message}</p>
+            )}
+          />
+          {validation.title && (
+            <p className='py-3 text-red-500'>{validation.title}</p>
+          )}
         </div>
         <div className='mb-5'>
           <div className='flex justify-start my-2'>
@@ -23,13 +112,26 @@ const Post: NextPage = () => {
           </div>
           <textarea
             className='p-2 border rounded-md w-full outline-none'
-            name='body'
             cols={30}
             rows={4}
+            {...register('body', { required: '必須入力です。' })}
           />
+          <ErrorMessage
+            errors={errors}
+            name={'body'}
+            render={({ message }) => (
+              <p className='py-3 text-red-500'>{message}</p>
+            )}
+          />
+          {validation.body && (
+            <p className='py-3 text-red-500'>{validation.body}</p>
+          )}
         </div>
         <div className='text-center'>
-          <button className='bg-gray-700 text-gray-50 py-3 sm:px-20 px-10 mt-8 rounded-xl cursor-pointer drop-shadow-md hover:bg-gray-600'>
+          <button
+            className='bg-gray-700 text-gray-50 py-3 sm:px-20 px-10 mt-8 rounded-xl cursor-pointer drop-shadow-md hover:bg-gray-600'
+            onClick={handleSubmit(createMemo)}
+          >
             登録する
           </button>
         </div>
